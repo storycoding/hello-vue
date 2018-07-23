d2c25a5df913fce6689f03ec8c424a76daccf0d3
storycoding <youknownuno@gmail.com>
2018-07-23 18:16:53 +0100

    created a child component that emits data to parent - needs fix

---
+++ src/components/Emitter.vue
@@ -0,0 +1,21 @@
+<template>
+ 	<button v-on:click="$emit('childEmission','1')">emit number to parent</button>
+</template>
+
+<script>
+
+	export default { 
+		name: 'Emitter'
+	}
+
+</script>
+
+
+<style scoped>
+
+	button {
+		background-color: blue;
+		color: white;
+	}
+
+</style>
