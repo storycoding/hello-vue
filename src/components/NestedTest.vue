8a597f5adad05f4a73aa28cedf795e616b6c6f1e
storycoding <youknownuno@gmail.com>
2018-07-23 11:08:02 +0100

    Added style

--- src/components/NestedTest.vue
+++ src/components/NestedTest.vue
@@ -1,5 +1,11 @@
 <template>
- 	<div>This is a nested component from a separate file</div>
+ 	<div id="NestedTest">This is a nested component from a separate file</div>
 </template>
+
+<style>
+	#NestedTest {
+		color: green;
+	}
+</style>
 
 export default { name: 'NestedTest' }
