6107db8ddb16aee6f2369dc1082718666fa198c9
storycoding <youknownuno@gmail.com>
2018-07-26 14:54:09 +0100

    created counter component for testing

---
+++ src/components/Counter.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+		<h3 data-testid='count'>{{count}}</h3>
+	</div>
+	
+</template>
+
+<script>
+	
+	export default {
+		name: 'Counter',
+		props : {
+			count : {
+				type : Number,
+				default	: 0
+			}	
+		}
+	}
+
+</script>
+
+<style scoped>
+
+	h3 {
+		color: blue;
+	}
+
+</style>
