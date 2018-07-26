490b64bc1a858ecd1f6e7a8bd6c12429667f3267
storycoding <youknownuno@gmail.com>
2018-07-26 16:21:24 +0100

    added increment and decrement buttons

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,19 +1,28 @@
 <template>
 	<div>
 		<h3 data-testid='count'>{{count}}</h3>
+		<button v-on:click="increment()" data-testid='increment'>+</button>
+		<button v-on:click="decrement()" data-testid='decrement'>-</button>
 	</div>
-	
 </template>
 
 <script>
 	
 	export default {
 		name: 'Counter',
-		props : {
+		props: {
 			count : {
 				type : Number,
 				default	: 0
 			}	
+		},
+		methods: {
+			increment: function() {
+				this.count ++
+			},
+			decrement: function() {
+				this.count --
+			}
 		}
 	}
 
